90cc26fd6f28bb2c8bbe49b886a8c6a5c8207428
J <oncwnuAtbq7RWXrDbsrMJw4vPdjA@git.weixin.qq.com>
2022-04-24 17:42:30 +0800

    更新

--- 03_webpack_ts/05_泛型/04_泛型类.ts
+++ 03_webpack_ts/05_泛型/04_泛型类.ts
@@ -1,21 +1,25 @@
 (()=>{
-    class GenericNumber<T> {
-        zeroValue: T 
-        add: (x: T, y: T) => T
+    // 定义一个类，类中的属性值的类型是不确定，方法中的参数及返回值的类型也是不确定
+    // 定义一个泛型类
+    class GenericNumber<T>{
+        // 默认的属性的值的类型是泛型类型
+        defaultValue:T
+        add:(x: T, y: T ) => T
     }
+    
 
-    let myGenericNumber = new GenericNumber<number>()
-    myGenericNumber.zeroValue = 0
-    myGenericNumber.add = function (x, y) {
+    const g1: GenericNumber<number> = new GenericNumber<number>()
+    g1.defaultValue = 100
+    g1.add = function(x, y){
         return x + y
     }
 
-    let myGenericString = new GenericNumber<string>()
-    myGenericString.zeroValue = 'abc'
-    myGenericString.add = function (x, y) {
+    console.log(g1.add(10,20))
+    const g2: GenericNumber<string> = new GenericNumber<string>()
+    g2.defaultValue = 'jack'
+    g2.add = function (x, y) {
         return x + y
     }
 
-    console.log(myGenericString.add(myGenericString.zeroValue, 'test'))
-    console.log(myGenericNumber.add(myGenericNumber.zeroValue, 12))
+    console.log(g2.add('诸葛', '孔明'))
 })()
--- 03_webpack_ts/src/main.ts
+++ 03_webpack_ts/src/main.ts
@@ -16,8 +16,8 @@
 // import './04_函数重载'
 // import './01_泛型'
 // import './02_多个泛型参数的函数'
-// import './03_泛型接口'
-// import './04_泛型类'
+// import '../05_泛型/03_泛型接口'
+// import '../05_泛型/04_泛型类'
 // import './05_泛型约束'
 
 document.write('webpack & ts')
